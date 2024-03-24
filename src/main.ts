4ce27a2b03368764e8fcde600939c24bc924a74f
Galang Ardian <ardiangalang05@gmail.com>
2024-03-25 04:30:55 +0800

    temp: show log environtment when start

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,16 @@ async function bootstrap() {
   const env = app.get(ConfigService);
   const port = env.get('PORT') || 3000;
 
+  console.log({
+    PORT: env.get('PORT'),
+    DB_HOST: env.get('DB_HOST'),
+    DB_PORT: env.get('DB_PORT'),
+    DB_NAME: env.get('DB_NAME'),
+    DB_USERNAME: env.get('DB_USERNAME'),
+    DB_PASSWORD: env.get('DB_PASSWORD'),
+    DB_TYPE: env.get('DB_TYPE'),
+  });
+
   app.enableCors();
   app.setGlobalPrefix('/api');
 
